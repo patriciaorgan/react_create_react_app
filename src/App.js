78208b9a711c05ecaa2bc4c41b3615aabb7875d9
Patricia Organ <patriciaorgan82@gmail.com>
2017-01-19 11:14:29 -0600

    React tutorial end part 2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+
+const App = () => {
+  return (
+    <div className="App">
+    <HelloWorld name="Jim"/>
+    <HelloWorld name="Sally"/>
+    </div>
+  );
+};
+
+export default App;
